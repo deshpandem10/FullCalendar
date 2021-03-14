6be4a30b5fcc6b10dbefff55c4d399c1da50455c
deshpandem10 <deshpandem10@gmail.com>
2021-03-14 20:20:49 +0400

    Scheduled tasks display section, enhancement.

--- index.js
+++ index.js
@@ -328,8 +328,8 @@ $(document).ready(function() {
             if((myMap.size != 0) && (getDataOFMap != undefined)) {              
                 $('#scheduledTasks').html('');
                 for(i = 0; i < getDataOFMap.length; i++) {
-                    if(getDataOFMap[i].endTime) {              // check if the element "end" of the object is present, if yes, then it is not All Day event
-                        $('#scheduledTasks').append(
+                    if(getDataOFMap[i].endTime) {              // check if the element "end" of the object is present, if yes, then it is not All Day event          
+                     /*   $('#scheduledTasks').append(
                             '<div class="col-sm-4">' +
                                 '<p class="scheduled_tasks_label">Title:  </p>' + 
                                 '<p class="scheduled_tasks_label">Start time:  </p>' + 
@@ -346,16 +346,43 @@ $(document).ready(function() {
                                 '<p class="scheduled_tasks_details">' + getLocationFromMap[i] + '<p>' +
                                 '<p class="scheduled_tasks_details">' + getDescriptionFromMap[i] + '<p>' + '<br/><br/>' +
                             '</div>' 
+                        );*/
+
+                        $('#scheduledTasks').append(
+                            '<div class="row scheduledTasks_display_row">' +
+                                '<div class="col-sm-4 scheduled_tasks_label">Title: </div>' +
+                                '<div class="col-sm-8 scheduled_tasks_details">' + getDataOFMap[i].title + '</div>' + 
+                            '</div>' +
+                            '<div class="row scheduledTasks_display_row">' +
+                                '<div class="col-sm-4 scheduled_tasks_label">Start Time: </div>' +
+                                '<div class="col-sm-8 scheduled_tasks_details">' + getDataOFMap[i].startTime + '</div>' + 
+                            '</div>' +
+                            '<div class="row scheduledTasks_display_row">' +
+                                '<div class="col-sm-4 scheduled_tasks_label">End Time: </div>' +
+                                '<div class="col-sm-8 scheduled_tasks_details">' + getDataOFMap[i].endTime + '</div>' + 
+                            '</div>' +
+                            '<div class="row scheduledTasks_display_row">' +
+                                '<div class="col-sm-4 scheduled_tasks_label">Participants: </div>' +
+                                '<div class="col-sm-8 scheduled_tasks_details" style="overflow-x: auto;">' + getParticipantsFromMap[i] + '</div>' + 
+                            '</div>' +
+                            '<div class="row scheduledTasks_display_row">' +
+                                '<div class="col-sm-4 scheduled_tasks_label">Location: </div>' +
+                                '<div class="col-sm-8 scheduled_tasks_details">' + getLocationFromMap[i] + '</div>' + 
+                            '</div>' +
+                            '<div class="row scheduledTasks_display_row">' +
+                                '<div class="col-sm-4 scheduled_tasks_label">Description: </div>' +
+                                '<div class="col-sm-8 scheduled_tasks_details">' + getDescriptionFromMap[i] + '</div>' + 
+                            '</div>' 
                         );
                     }
-                    else {
+                    else {             
                         $('#scheduledTasks').append(
                             '<b>Title:  </b>' + "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"+'<span style="color:#2196f3;">'+  getDataOFMap[i].title + '</span>'+'<br/>' +
                             '<b>Full Day:  </b>' + "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"+'<span style="color:#2196f3;">' +" Yes " +'</span>' + '<br/>' +
                             '<b>Participants:  </b>' + "&nbsp;&nbsp;"+'<span style="color:#2196f3;">' + getParticipantsFromMap[i]  +'</span>'+ '<br/>' +
                             '<b>Location:  </b>' + "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"+'<span style="color:#2196f3;">' + getLocationFromMap[i]  +'</span>'+ '</br>' +
                             '<b>Description:  </b>' + "&nbsp;&nbsp;&nbsp;"+'<span style="color:#2196f3;">' + getDescriptionFromMap[i]  +'</span>'+'<br/><br/>'
-                        );
+                        );       
                     }
                 }              
             }
